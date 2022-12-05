2326036cf9765b28c6fa542c4db5681f46239db2
khr0x40sh <khr0x40sh@gmail.com>
2022-12-05 08:49:11 -0500

    Update mmm_mining_utility.c
    RE-worked fgets to accept access-code properly

--- mmm_mining_utility.c
+++ mmm_mining_utility.c
@@ -6,64 +6,66 @@
 #include<stdlib.h>    
 
 void debug_console() {
-	printf("[!] Success, grab your flag by executing cat flag.txt\n");
-	fflush(stdout);
-	system("/bin/sh");
+        printf("[!] Success, grab your flag by executing cat flag.txt\n");
+        fflush(stdout);
+        system("/bin/sh");
 }
 
-int check_access(char access[41])
+int check_access(char access[42])
 {
-	char input[2000];
-	printf("[!] Please enter the access code> ");
-	fflush(stdout);
-	fgets(input,2000,stdin);
+        char input[42];
+        printf("[!] Please enter the access code> ");
+        fflush(stdout);
+        fgets(input,42,stdin);
 
-	if (strcmp(input, access) == 0)
-	{
-		printf("[+] Access code accepted!!!\n ");
-		printf("[+] Access granted!\n");
-		fflush(stdout);
-		return 1;
-	}
-	else
-	{
-		printf("[-] Incorrect access code, Goodbye!!!\n ");
-		fflush(stdout);
-		return 0;
-	}
+        if (strcmp(input, access) == 0)
+        {
+                printf("[+] Access code accepted!!!\n ");
+                printf("[+] Access granted!\n");
+                fflush(stdout);
+                return 1;
+        }
+        else
+        {
+                printf("[-] Incorrect access code, Goodbye!!!\n ");
+                fflush(stdout);
+                return 0;
+        }
 
 }
 
 void get_hash(char input[512])
 {
-	char copied[128];
+        char copied[128];
 
-	printf("[+] Copying your input to hash checker...\n  ");
-	fflush(stdout);
-	strcpy(copied, input);
+        printf("[+] Copying your input to hash checker...\n  ");
+        fflush(stdout);
+        strcpy(copied, input);
 }
 
 int main(int argc, char* argv[])
-{	
-	char input[512];
-	char access_code[41] = "f39c7e0a6ff1b55d8a9cc72af6b38f4a126d62c6";
-	
-	/*if(argc < 2)
-	{
-		printf("\nUSAGE: %s <hash>\n\n",argv[0]);
-		return -1;
-	}*/
-	
-	if(check_access(access_code))
-	{
-		printf("=============================================\n");
-		printf("   MMM Mining Utility v0.1a \n");
-		printf("============================================= \n");
-		printf("\n");
-		printf("[!] Insert your hash> ");
-		fflush(stdout);
-		fgets(input,512,stdin);
-		get_hash(input);
-	}
-	return 0;
+{
+        char input[512];
+        char access_code[42] = "f39c7e0a6ff1b55d8a9cc72af6b38f4a126d62c6\n";
+
+        /*if(argc < 2)
+        {
+                printf("\nUSAGE: %s <hash>\n\n",argv[0]);
+                return -1;
+        }*/
+
+        if(check_access(access_code))
+        {
+                printf("=============================================\n");
+                printf("   MMM Mining Utility v0.1a \n");
+                printf("============================================= \n");
+                printf("\n");
+                printf("[!] Insert your hash> ");
+                fflush(stdout);
+                fflush(stdout);
+                fgets(input,512,stdin);
+                printf(input);
+                get_hash(input);
+        }
+        return 0;
 }
